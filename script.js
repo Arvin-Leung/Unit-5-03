105f2392c7b1d31b605be3eee1cda0278fd494d4
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-11 14:05:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// This line calls the Function
+document.getElementById('alert').addEventListener("click", myFunction)
+
+// This function causes the alert to appear
+function myFunction() {
+  alert("Hello, World!")
+}
